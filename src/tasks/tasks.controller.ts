4b62e3acbd741fd7810fd7ec4ac2f2b072010dac
Krishna <kish77na@gmail.com>
2024-01-25 19:33:55 +0530

    Added update status and filter to getTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Get, Post, Body, Param, Delete } from "@nestjs/common";
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
-import { Task } from "./tasks.model";
+import { Task, TaskStatus } from "./tasks.model";
 import { CreateTask } from "./dto/createTask.dto";
+import { GetTaskFilter } from "./dto/get-task-filter.dto";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getTasks(): Task[] {
-    console.log("Getting tasks");
-    return this.taskService.getAllTasks();
+  getTasks(@Query() filterDto: GetTaskFilter): Task[] | string{
+
+    // If filter defined call by filter
+    if(Object.keys(filterDto).length){
+      return this.taskService.getTaskFiltered(filterDto)
+    }else{
+      // If filter not defined call getAllTasks()
+      return this.taskService.getAllTasks();
+    }
   }
 
   @Post()
@@ -29,5 +36,15 @@ export class TasksController {
   deleteTaskById(@Param('id') id: string): string{
     console.log(this.taskService.deleteTaskById(id));
     return 'deleted'
+  }
+
+  @Patch('/:id')
+  patchTaskById(@Param('id') id: string, @Body('status') status: TaskStatus): Task | string{
+    if(Object.values(TaskStatus).includes(status)){
+      const updatedTask = this.taskService.updateStatusById(id, status);
+      console.log('This is patched', id, 'This is status', status);
+      return updatedTask;
+    }
+    return 'Invalid Status'
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import { Task, TaskStatus } from "./tasks.model";
 // a module to auto-generate id
 import { v4 as uuid } from "uuid";
 import { CreateTask } from "./dto/createTask.dto";
+import { GetTaskFilter } from "./dto/get-task-filter.dto";
 
 @Injectable()
 export class TasksService {
@@ -11,6 +12,29 @@ export class TasksService {
   getAllTasks(): Task[] {
     return this.input;
   }
+
+  getTaskFiltered(filterDto: GetTaskFilter): Task[]{
+    const { search, status } = filterDto;
+    console.log('::::Also Reached Here::::');
+
+    let copyList: Task[] = this.input;
+
+    if(status){
+      copyList = copyList.filter((task) => {
+        return task.status === status
+      })
+    }
+    if(search){
+      copyList = copyList.filter((task) =>{
+        if(task.title.includes(search) || task.description.includes(search))
+        return true;
+      else
+      return false;
+      })
+    }
+    return copyList;
+
+    }
 
   createTask(createTask: CreateTask): Task {
     const { title, description } = createTask;
@@ -40,4 +64,16 @@ export class TasksService {
     console.log('removed Input:::', removedInput);
     return 'success';
   }
+
+  updateStatusById(taskId: string, status: TaskStatus): Task{
+      const taskToBeUpdated:Task = this.getTaskById(taskId);
+      taskToBeUpdated.status = status;
+
+      return taskToBeUpdated;
+  }
+
+  //   this.input.indexOf(taskToBeUpdated)
+
+  //   if(taskToBeUpdated.status === status)
+  // }
 }
